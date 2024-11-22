ba1031b6255a25a8225b0e29db5faef9f8a50df0
AlejandroAttento <alejandro.attento@gmail.com>
2024-11-22 19:36:21 -0300

    Update: .gitignore

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Medical LLM using Llama3',
+    version='1.0.0',
+    author="Alejandro Daniel Attento",
+    author_email="alejandro.attento@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
